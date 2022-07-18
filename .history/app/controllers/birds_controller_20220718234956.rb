170f1d325a485e1c24e002e8b78d03e00e4625ce
NDICHU <ndichumuriithi@gmail.com>
2022-07-19 00:08:28 +0300

    Completed assignments

--- .history/app/controllers/birds_controller_20220718234956.rb
+++ .history/app/controllers/birds_controller_20220718234956.rb
@@ -1,11 +1,5 @@
 class BirdsController < ApplicationController
 
-  # POST /birds
-  def create
-  bird = Bird.create(name: params[:name], species: params[:species])
-  render json: bird, status: :created
-  end
-
   # GET /birds
   def index
     birds = Bird.all
